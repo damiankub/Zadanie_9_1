bb8fcc21e68f61a918a4f32688ca42517bb484d9
Damian <damiankub89@gmail.com>
2017-09-28 13:20:51 +0200

    Traingle Area

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function getTriangleArea(a, h) {
+  if (a>0 && h>0) return a*h/2;
+  else console.log('Nieprawidłowe dane');
+}
+
+var triangle1Area1 = getTriangleArea(10, 6),
+	triangle1Area2 = getTriangleArea(10, 15),
+	triangle1Area3 = getTriangleArea(4, 5);
+console.log(triangle1Area1);
+console.log(triangle1Area2);
+console.log(triangle1Area3);
+
